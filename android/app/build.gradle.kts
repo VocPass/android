a38fcb977f6bd6791c5b1f65f19e3dd74d42eac4
Hans <hi@hans0805.me>
2026-04-20 10:00:10 +0800

    feat: update APK signing process and keystore handling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,11 +19,24 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val hasKeystore = keystorePropertiesFile.exists()
+
+    if (hasKeystore) {
+        val keystoreProperties = java.util.Properties()
+        keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+        signingConfigs {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "me.hans0805.vocpass"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +45,10 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = if (hasKeystore)
+                signingConfigs.getByName("release")
+            else
+                signingConfigs.getByName("debug")
         }
     }
 }
